8c736582d58ec7eb6f69fd8f5120240dfb752e8d
s.melnikov <s.melnikov@bpcbt.com>
2019-05-11 18:42:07 +0400

    Add missing files.

---
+++ machine-learning-ex6/classifySpam.m
@@ -0,0 +1,6 @@
+function classifySpam = classifySpam(filename, svmModel)
+  file_contents = readFile(filename);
+  word_indices  = processEmail(file_contents);
+  x             = emailFeatures(word_indices);
+  classifySpam  = svmPredict(svmModel, x);
+endfunction
